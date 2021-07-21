8cb5b00dd0a6ed05c6577ad17029ec3eff4570d5
ubermen5che <ubermen5ch1308@gmail.com>
2021-07-21 18:15:20 +0900

    크롤링 데이터 처리를 위한 DTO 추가

--- src/main/java/KM/KNUguard/domain/CrawlData.java
+++ src/main/java/KM/KNUguard/domain/CrawlData.java
@@ -1,13 +1,13 @@
 package KM.KNUguard.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
 
 public class CrawlData {
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String u_name;
     private String college;
@@ -16,10 +16,13 @@ public class CrawlData {
     private Timestamp exposure_sdate;
     private Timestamp exposure_edate;
     private String disinfection;
-    private Float latitude;
-    private Float longitude;
+    @Column(name = "latitude")
+    private Float lat;
+    @Column(name = "longitude")
+    private Float lng;
     private Timestamp crawled_date;
     private String content;
+    private String region;
 
     public Long getId() {
         return id;
@@ -77,20 +80,20 @@ public class CrawlData {
         this.disinfection = disinfection;
     }
 
-    public Float getLatitude() {
-        return latitude;
+    public Float getLat() {
+        return lat;
     }
 
-    public void setLatitude(Float latitude) {
-        this.latitude = latitude;
+    public void setLat(Float latitude) {
+        this.lat = latitude;
     }
 
-    public Float getLongitude() {
-        return longitude;
+    public Float getLng() {
+        return lng;
     }
 
-    public void setLongitude(Float longitude) {
-        this.longitude = longitude;
+    public void setLng(Float longitude) {
+        this.lng = longitude;
     }
 
     public Timestamp getCrawledDate() {
@@ -115,5 +118,45 @@ public class CrawlData {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getU_name() {
+        return u_name;
+    }
+
+    public void setU_name(String u_name) {
+        this.u_name = u_name;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public Timestamp getExposure_sdate() {
+        return exposure_sdate;
+    }
+
+    public void setExposure_sdate(Timestamp exposure_sdate) {
+        this.exposure_sdate = exposure_sdate;
+    }
+
+    public Timestamp getExposure_edate() {
+        return exposure_edate;
+    }
+
+    public void setExposure_edate(Timestamp exposure_edate) {
+        this.exposure_edate = exposure_edate;
+    }
+
+    public Timestamp getCrawled_date() {
+        return crawled_date;
+    }
+
+    public void setCrawled_date(Timestamp crawled_date) {
+        this.crawled_date = crawled_date;
     }
 }
